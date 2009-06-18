70a757aedb5bbddfff938743ea3c4715623d1ae6
kentaromiura <kentaromiura@gmail.com>
2009-06-18 19:46:54 +0200

    adding csharp project files commit

---
+++ lessCompiler/lessCompiler/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace lessCompiler {
+    class Program {
+
+        static void Main(string[] args) {
+
+            
+            /*
+            startConversion(@"c:\less\test1.less", @"c:\less\test1.css");
+            startConversion(@"c:\less\test2.less", @"c:\less\test2.css");
+            startConversion(@"c:\less\miotest.less", @"c:\less\miotest.css");
+            
+             */ 
+
+            // http://lesscss.org/index.html
+            if (args.Length < 1 || args.Length > 2) {
+                printCorrectSyntax();
+            } else {
+                string inputfile = args[0], outputfile = string.Format("{0}.css", inputfile);
+                if (args.Length > 1) {
+                    outputfile = args[1];
+                }
+                
+                startConversion(inputfile, outputfile);
+            }
+        }
+
+        private static void startConversion(string inputfile, string outputfile) {
+            string input = getInput(inputfile), output = lessConverter.Convert(input);
+            writeOutput(outputfile,output);
+        }
+
+        private static void writeOutput(string outputfile, string content) {
+            if (File.Exists(outputfile)) {
+                File.Delete(outputfile);
+            }
+
+            using (var output = File.CreateText(outputfile)) {
+                output.Write(content);
+                output.Flush();
+                output.Close();
+            }
+        }
+
+        private static string getInput(string inputfile) {
+            string input = "";
+
+            if (File.Exists(inputfile)) {            
+                using (var sr = File.OpenText(inputfile)) {
+                    input = sr.ReadToEnd();
+                }                
+            }
+
+            return input;
+        }
+
+        private static void printCorrectSyntax() {
+            string executablename = Assembly.GetExecutingAssembly().GetName().Name;
+            Console.WriteLine("{0} inputfile [outputfile]",executablename);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ lessCompiler/lessCompiler/lessConverter.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace lessCompiler {
+   public class lessConverter {
+
+        public static string Convert(string less) {
+            string output = replaceConstant(less);
+            output = replaceMixins(output);
+            return output.Trim();        
+        }
+
+        private static string prepareMixins(string input) {
+            Regex rg = new Regex(@"\..+?;");
+            Regex spaceBeforeSemicolon = new Regex(@"\s+?;");
+            return rg.Replace(input, (match) =>
+            {
+                return spaceBeforeSemicolon.Replace(match.Value, ";");
+            });
+        }
+
+        private static MatchEvaluator splitAndRemove(char separator, char remove, string returnFormat,IDictionary<string, string> repository) {
+            return (match) =>
+            {
+                string[] namevalue = match.Value.Split(separator);
+                string value = namevalue[1];
+                repository.Add(namevalue[0].Trim(), value.Remove(value.LastIndexOf(remove)).Trim());
+                return string.Format(returnFormat, match.Value);
+            };
+        }
+
+        private static string replaceMixins(string input) {
+            IDictionary<string, string> rules = new Dictionary<string, string>();
+
+            Regex rg = new Regex(@"\..+?\s*?\{\s*?(.|\s)+?\}");
+            rg.Replace(input, splitAndRemove('{', '}', "{0}", rules));
+
+            string mixinPrepared = prepareMixins(input);
+            return replaceMixinsWithRules(mixinPrepared, rules);
+        }
+
+
+        private static string replaceConstant(string input) {
+            IDictionary<string, string> constants = new Dictionary<string, string>();
+            Regex rg = new Regex(@"@\s*?[\w-]+?\s*?:\s*?.*?\s*?;");
+            string inputWithoutConstantsDefinition = rg.Replace(input,splitAndRemove(':',';',"",constants));
+
+            return replaceConstantWithValues(inputWithoutConstantsDefinition, constants);
+        }
+
+        private static string replaceKeysWithValues(string input, IDictionary<string, string> dictionary, string inputFormat, string outputFormat) {
+            string finalString = input;
+            foreach (string keys in dictionary.Keys) {
+                finalString = finalString.Replace(string.Format(inputFormat, keys), string.Format(outputFormat, dictionary[keys]));
+            }
+            return finalString;
+        }
+
+        private static string replaceMixinsWithRules(string input, IDictionary<string, string> rules) {
+            return replaceKeysWithValues(input, rules, "{0};", "{0}");
+        }
+
+        private static string replaceConstantWithValues(string inputWithoutConstantsDefinition, IDictionary<string, string> constants) {
+            return replaceKeysWithValues(inputWithoutConstantsDefinition, constants, "{0};", "{0};");
+        }
+
+    }
+}
